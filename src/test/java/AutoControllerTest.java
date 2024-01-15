ed6f46f3bef982a982b905a00b3b209fa92a804e
sgosta <sgosta@eagleprojects.it>
2024-01-15 12:49:45 +0100

    commentato tutto il test

--- src/test/java/AutoControllerTest.java
+++ src/test/java/AutoControllerTest.java
@@ -1,47 +1,46 @@
-import it.example.config.AppConfig;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import static org.junit.Assert.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.SharedHttpSessionConfigurer.sharedHttpSession;
-
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {AppConfig.class})
-public class AutoControllerTest {
-
-    @Autowired
-    WebApplicationContext context;
-
-    MockMvc mockMvc;
-
-    @Before
-    public void setUp() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
-                .defaultRequest(get("/").accept(MediaType.APPLICATION_JSON))
-                .apply(sharedHttpSession()).alwaysExpect(status().isOk())
-                .alwaysExpect(content().contentType("application/json;charset=UTF-8"))
-                .build();
-    }
-
-    @Test
-    public void testOnLaunchGet() throws Exception {
-        MvcResult result = this.mockMvc.perform(get("/")
-                .accept(MediaType.APPLICATION_JSON)).andReturn();
-        assertEquals("OK", result.getResponse().getContentAsString());
-    }
-}
+//import it.example.config.AppConfig;
+//import org.junit.Before;
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.event.annotation.BeforeTestClass;
+//import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.MvcResult;
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+//import org.springframework.web.context.WebApplicationContext;
+//import static org.junit.Assert.assertEquals;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//import static org.springframework.test.web.servlet.setup.SharedHttpSessionConfigurer.sharedHttpSession;
+//
+//@RunWith(SpringRunner.class)
+//@ContextConfiguration(classes = {AppConfig.class})
+//public class AutoControllerTest {
+//
+//    @Autowired
+//    WebApplicationContext context;
+//
+//    MockMvc mockMvc;
+//
+//    @Before
+//    public void setUp() {
+//        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+//                .defaultRequest(get("/").accept(MediaType.APPLICATION_JSON))
+//                .apply(sharedHttpSession()).alwaysExpect(status().isOk())
+//                .alwaysExpect(content().contentType("application/json;charset=UTF-8"))
+//                .build();
+//    }
+//
+//    @Test
+//    public void testOnLaunchGet() throws Exception {
+//        MvcResult result = this.mockMvc.perform(get("/")
+//                .accept(MediaType.APPLICATION_JSON)).andReturn();
+//        assertEquals("OK", result.getResponse().getContentAsString());
+//    }
+//}
